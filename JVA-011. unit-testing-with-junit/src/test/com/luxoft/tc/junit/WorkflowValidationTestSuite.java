999c3925bfdbad562bb61617090085c9c853186a
Andrey Parkhomenko <andr.parkhomenko@gmail.com>
2016-07-28 23:36:05 +0200

    Update of the slides

--- JVA-011. unit-testing-with-junit/src/test/com/luxoft/tc/junit/WorkflowValidationTestSuite.java
+++ JVA-011. unit-testing-with-junit/src/test/com/luxoft/tc/junit/WorkflowValidationTestSuite.java
@@ -4,6 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ PersonFieldsValidatorTest.class, PersonBehaviorValidatorTest.class})
+@Suite.SuiteClasses({
+        PersonFieldsValidatorTest.class,
+        PersonBehaviorValidatorTest.class})
 public class WorkflowValidationTestSuite {
+    // the class remains empty,
+    // used only as a holder for the above annotations
 }
